27c26261a1dae2c250f5a2401ca3de7116e2a685
clipscomb <canadiancookies01@gmail.com>
2024-01-30 18:31:53 -0500

    basic calc unfinished

--- week 1/BasicCalculator/src/BasicCalculator.java
+++ week 1/BasicCalculator/src/BasicCalculator.java
@@ -1,7 +1,60 @@
+import java.util.Scanner;
+
 public class BasicCalculator {
     public static void main(String[] args) {
-        int firstNumber = 5;
-        int secondNumber = 12;
-    }
 
-}
+        //declare variable
+        Scanner userInput = new Scanner(System.in);
+        String type;
+        int firstNumber;
+        int secondNumber;
+        double totalNum;
+
+
+
+        //input
+        System.out.println("First Number");
+        firstNumber = userInput.nextInt();
+        System.out.println("Second Number");
+        secondNumber = userInput.nextInt();
+        System.out.println("Please select operation: ");
+        type = userInput.nextLine();
+
+
+        System.out.println("(A)dd");
+        System.out.println("(s)ubtract");
+        System.out.println("(m)ultiply");
+        System.out.println("(d)ivide");
+
+
+        if (type.equalsIgnoreCase("")) {
+            totalNum = firstNumber + secondNumber;
+            System.out.println("Order total: $" + totalNum);}
+
+
+     if (type.equalsIgnoreCase("")) {
+        totalNum = firstNumber - secondNumber;
+        System.out.println("Order total: $" + totalNum);
+     }
+
+     if (type.equalsIgnoreCase("")) {
+        totalNum = firstNumber * secondNumber;
+        System.out.println("Order total: $" + totalNum);
+    }
+     if (type.equalsIgnoreCase("")) {
+           totalNum =(firstNumber / secondNumber);
+            System.out.println("Order total: $" + totalNum);
+        }
+
+}}
+
+
+
+
+
+
+
+
+
+
+        //System.out.println("Answer");
